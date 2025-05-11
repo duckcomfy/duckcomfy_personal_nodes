905173da6f10b87a4cd902771d408b140612ed4e
duckcomfy <a@a.a>
2025-05-11 16:14:54 +0200

    v4

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __all__ = [
 
 __author__ = """duckcomfy"""
 __email__ = "juan.hernandez.1974@proton.me"
-__version__ = "3.0.0"
+__version__ = "4.0.0"
 
 from .src.duckcomfy_personal_nodes.nodes import NODE_CLASS_MAPPINGS
 from .src.duckcomfy_personal_nodes.nodes import NODE_DISPLAY_NAME_MAPPINGS
